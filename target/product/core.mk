078723960e6dc2ab4a969b57e81170f73157dea2
nisarg <nisarg@cs.duke.edu>
2018-10-21 18:22:09 -0400

    Add PermissionsPluginController app to the make file
    Change-Id: Iefb9ec78ed7f9d9495bb96a1690fa71580881da7

--- target/product/core.mk
+++ target/product/core.mk
@@ -59,7 +59,8 @@ PRODUCT_PACKAGES += \
     TeleService \
     VpnDialogs \
     vr \
-    MmsService
+    MmsService \
+    PermissionsPluginController 
 
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \
